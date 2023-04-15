b56d06d7910634898486f017640bfb7b3bdc65bb
Yiğit Erdinç <yigiterdinc@gmail.com>
2023-04-15 16:41:41 +0300

    Initial Commit

---
+++ Scorp-Case-Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scorp-Case-Study
+//
+//  Created by Yiğit Erdinç on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
